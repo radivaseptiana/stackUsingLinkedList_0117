a86f11f49399de67a683db0efa8373163adf466e
radivaseptiana <radivaseptiana51@gmail.com>
2025-06-05 15:25:13 +0700

    return the value of the top node

--- stack.cpp
+++ stack.cpp
@@ -62,31 +62,18 @@ class Stack
         {
             cout << "List is empety." << endl;
             return; // if the stack is empety, print a message and return
-      
+        }
+        else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
         } //Return the value of the top node
-    }
-};
-int main()
-{
-    Stack stack;
 
-    int choice = 0;
-    int value;
-
-    while (choice !=4)
-    {
-        cout << "1. Push\n";
-        cout << "2. Pop\n";
-        cout << "3. Peek\n";
-        cout << "4. Exit\n";
-        cout << "Enter your choice: ";
-        cin >> choice;
-
-       switch (choice)
-       {
-        case 1:
-        cout << "Enter the value to push: ";
-        cin >> value;
         stack.push(value); // Push the entered value into the stack
         break;
         case 2:
