9e953dc9d0acb6e5201c5338d3aa2598144a14f4
radivaseptiana <radivaseptiana51@gmail.com>
2025-06-05 15:10:00 +0700

    Push the entered value into the stack

--- stack.cpp
+++ stack.cpp
@@ -74,4 +74,29 @@ class Stack
             cout << endl;
         } //Return the value of the top node
     }
+};
+int main()
+{
+    Stack stack;
+
+    int choice = 0;
+    int value;
+
+    while (choice !=4)
+    {
+        cout << "1. Push\n";
+        cout << "2. Pop\n";
+        cout << "3. Peek\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+       switch (choice)
+       {
+        case 1:
+        cout << "Enter the value to push: ";
+        cin >> value;
+        stack.push(value); // Push the entered value into the stack
+       }
+    }
 }
