ac870077938951f45a4c0e2a00e4c9398513d33e
radivaseptiana <radivaseptiana51@gmail.com>
2025-06-05 15:38:15 +0700

    Create a temporary pointer to the current top node

--- stack.cpp
+++ stack.cpp
@@ -46,6 +46,7 @@ class Stack
     // Pop operation: Remove the topmost element from the stack
     void pop()
     {
+        Node* temp = top; // Create a temporary pointer to the current top node
         if (isEmpety())
         {
             cout << "Stack is empety." << endl;
@@ -73,7 +74,29 @@ class Stack
             }
             cout << endl;
         } //Return the value of the top node
+    }
+};
+int main()
+{
+    Stack stack;
 
+    int choice = 0;
+    int value;
+
+    while (choice !=4)
+    {
+        cout << "1. Push\n";
+        cout << "2. Pop\n";
+        cout << "3. Peek\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+       switch (choice)
+       {
+        case 1:
+        cout << "Enter the value to push: ";
+        cin >> value;
         stack.push(value); // Push the entered value into the stack
         break;
         case 2:
